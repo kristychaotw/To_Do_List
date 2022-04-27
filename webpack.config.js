7dd461edb9f1526ad07eeadf5f3c31e4d1b4cef6
kristy chao <kristy334420@gmail.com>
2022-04-27 09:30:40 +0800

    [update]: webpack settings add css & html plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
- module: {
-   rules: [
-     {
-       test: /\.css$/i,
-       use: ['style-loader', 'css-loader'],
-     },
-     {
-        test: /\.js$/ ,
-        use: ['babel-loader'],
-      },
-   ],
- },
+    entry: './src/index.js',
+    output: {
+        filename: 'main.[hash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [{
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.js$/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './base.html'
+    }), new MiniCssExtractPlugin({
+        filename:'main.[hash].css'
+    })],
 };
